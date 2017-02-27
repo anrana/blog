c344d905f727a8582abb5d383c9a7d8806678a65
anrana <962416108@qq.com>
2017-02-27 20:18:53 +0800

    2017.2.27

---
+++ js/1.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('#flashbox').smallslider({
+        switchEffect: 'ease',
+        switchEase: 'easeOutBounce',
+        switchPath: 'up',
+        textPosition: 'top',
+        textAlign: 'center',
+        textSwitch: 2
+    });
+});
